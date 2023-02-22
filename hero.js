893f8b325140e6cf9ef669c08b222883d5aa9c64
Jeet-Pattani <93391700+Jeet-Pattani@users.noreply.github.com>
2023-02-23 00:42:28 +0530

    Update 1.1

---
+++ hero.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    strings: ["Comfort","Style","Confidence","Empowerment","Radiance"],
+    typeSpeed: 45,
+    backSpeed: 15,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false,
+    smartBackspace: true
+});
--- welcome.js
+++ welcome.js
@@ -58,11 +58,11 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 //Paragraphs
-	anime({
-		targets: 'p',
-  	opacity: 1,
-  	duration: 1800,
-  	translateY: ["-30px", "0px"],
-  	easing: "easeOutExpo",
-		delay: (el, i) => 5200 + 100 * i,
-	})
+	// anime({
+	// targets: 'p',
+  	// opacity: 1,
+  	// duration: 1800,
+  	// translateY: ["-30px", "0px"],
+  	// easing: "easeOutExpo",
+	// 	delay: (el, i) => 5200 + 100 * i,
+	// })
